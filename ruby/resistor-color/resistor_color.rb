6f758a7b95e2f2b9d086a3f309c9eb87939c7b57
Zasda Yusuf Mikail <zasdaym@gmail.com>
2020-03-08 11:34:25 +0700

    Initial commit

---
+++ ruby/resistor-color/resistor_color.rb
@@ -0,0 +1,28 @@
+=begin
+Write your code for the 'Resistor Color' exercise in this file. Make the tests in
+`resistor_color_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/resistor-color` directory.
+=end
+
+class ResistorColor
+  @@colors = {
+    'black'  => 0,
+    'brown'  => 1,
+    'red'    => 2,
+    'orange' => 3,
+    'yellow' => 4,
+    'green'  => 5,
+    'blue'   => 6,
+    'violet' => 7,
+    'grey'   => 8,
+    'white'  => 9,
+  }
+
+  COLORS = @@colors.keys
+
+  def self.color_code(color)
+    @@colors[color]
+  end
+end

cafcdd6c540ea0ca6bc528945366272300a5e401
Zasda Yusuf Mikail <zasdaym@gmail.com>
2020-03-12 08:57:42 +0700

    Update go solutions

---
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -0,0 +1,24 @@
+// Solution for problem Collatz Conjecture
+package collatzconjecture
+
+import (
+	"errors"
+)
+
+// CollatzConjecture will return total steps of a collatz conjecture of a given integer
+func CollatzConjecture(n int) (int, error) {
+	if n < 1 {
+		return 0, errors.New("initial number must be equal or more than 1")
+	}
+
+	count := 0
+	for n != 1 {
+		if n % 2 == 0 {
+			n /= 2
+		} else {
+			n = n * 3 + 1
+		}
+		count++
+	}
+	return count, nil
+}

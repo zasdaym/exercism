a98ddd311619b71a2bff84704183e2ae74cf3e48
Zasda Yusuf Mikail <zasdaym@gmail.com>
2020-03-23 12:20:32 +0700

    Add python ISBN Verifier solution

---
+++ python/isbn-verifier/isbn_verifier.py
@@ -0,0 +1,20 @@
+def is_valid(isbn):
+    splitted = list("".join(isbn.split("-")))
+    if len(splitted) != 10 or not "".join(splitted[:-1]).isdigit():
+        return False
+
+    if not splitted[-1].isdigit() and splitted[-1] != "X":
+        return False
+
+    sum = 0
+    for i in range(9):
+        print(f"{splitted[i]} * {10-i}")
+        sum += int(splitted[i]) * (10-i)
+
+    if splitted[-1] == "X":
+        sum += 10
+    else:
+        sum += int(splitted[-1])
+
+    print(f"sum is {sum} mod is {sum % 11}")
+    return sum % 11 == 0

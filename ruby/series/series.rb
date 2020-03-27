d5a00d19e91ba0356565284a6b3367c3927b5678
Zasda Yusuf Mikail <zasdaym@gmail.com>
2020-03-27 08:25:11 +0700

    Update ruby Series solution

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,15 +1,15 @@
 class Series
-  def initialize(s)
-    @s = s.chars
+  def initialize(series)
+    @series = series.chars
   end
 
-  def slices(n)
-    if @s.length < n
-      raise ArgumentError.new("Number exceeds series length")
+  def slices(length)
+    if @series.length < length
+      raise ArgumentError.new("Slice length exceeds series length")
     end
 
     result = []
-    (@s.length - n + 1).times { |i| result.append(@s[i, n].join) }
+    @series.each_cons(length) { |slice| result.append(slice.join) }
     result
   end
 end

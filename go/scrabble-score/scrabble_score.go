f9996f082e88620f9bf5afe849ad79fac6573ba7
Zasda Yusuf Mikail <zasdaym@gmail.com>
2020-03-29 19:49:47 +0700

    Update go Scrabble Score solution

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -3,39 +3,39 @@ package scrabble
 
 import "unicode"
 
+var scores = map[rune]int{
+	'A': 1,
+	'B': 3,
+	'C': 3,
+	'D': 2,
+	'E': 1,
+	'F': 4,
+	'G': 2,
+	'H': 4,
+	'I': 1,
+	'J': 8,
+	'K': 5,
+	'L': 1,
+	'M': 3,
+	'N': 1,
+	'O': 1,
+	'P': 3,
+	'Q': 10,
+	'R': 1,
+	'S': 1,
+	'T': 1,
+	'U': 1,
+	'V': 4,
+	'W': 4,
+	'X': 8,
+	'Y': 4,
+	'Z': 10,
+}
+
 // Score will return the scrabble score of the given word.
 func Score(s string) int {
 	if s == "" {
 		return 0
-	}
-
-	scores := map[rune]int{
-		'A': 1,
-		'B': 3,
-		'C': 3,
-		'D': 2,
-		'E': 1,
-		'F': 4,
-		'G': 2,
-		'H': 4,
-		'I': 1,
-		'J': 8,
-		'K': 5,
-		'L': 1,
-		'M': 3,
-		'N': 1,
-		'O': 1,
-		'P': 3,
-		'Q': 10,
-		'R': 1,
-		'S': 1,
-		'T': 1,
-		'U': 1,
-		'V': 4,
-		'W': 4,
-		'X': 8,
-		'Y': 4,
-		'Z': 10,
 	}
 
 	score := 0

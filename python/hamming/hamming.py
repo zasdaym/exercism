8678b4b77e2bfad7e86ee841bbcbde9a4d08d388
Zasda Yusuf Mikail <zasdaym@gmail.com>
2020-04-11 17:07:28 +0700

    Refactor python Hamming solution

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -2,9 +2,4 @@ def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("strands length not same!")
 
-    count = 0
-    for i in range(len(strand_a)):
-        if strand_a[i] != strand_b[i]:
-            count += 1
-
-    return count
+    return sum(strand_a[i] != strand_b[i] for i in range(len(strand_a)))

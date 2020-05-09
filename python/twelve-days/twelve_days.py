9dee2ce83cd62ab1b50c0cd7d2af797454dc15fe
Zasda Yusuf Mikail <zasdaym@gmail.com>
2020-05-10 06:31:55 +0700

    Update python Twelve Days solution

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,18 +1,30 @@
-song = {1: ['first', 'a Partridge in a Pear Tree.'], 2: ['second','two Turtle Doves, and'],
-        3: ['third', 'three French Hens,'], 4: ['fourth', 'four Calling Birds,'],
-        5: ['fifth', 'five Gold Rings,'], 6: ['sixth', 'six Geese-a-Laying,'], 
-        7: ['seventh', 'seven Swans-a-Swimming,'], 8: ['eighth', 'eight Maids-a-Milking,'], 
-        8: ['eighth', 'eight Maids-a-Milking,'], 9: ['ninth', 'nine Ladies Dancing,'], 
-        10: ['tenth', 'ten Lords-a-Leaping,'], 11: ['eleventh', 'eleven Pipers Piping,'], 
-        12: ['twelfth', 'twelve Drummers Drumming,']}
+poems = [
+    ("first", "a Partridge in a Pear Tree."),
+    ("second", "two Turtle Doves, "),
+    ("third", "three French Hens, "),
+    ("fourth", "four Calling Birds, "),
+    ("fifth", "five Gold Rings, "),
+    ("sixth", "six Geese-a-Laying, "),
+    ("seventh", "seven Swans-a-Swimming, "),
+    ("eighth", "eight Maids-a-Milking, "),
+    ("ninth", "nine Ladies Dancing, "),
+    ("tenth", "ten Lords-a-Leaping, "),
+    ("eleventh", "eleven Pipers Piping, "),
+    ("twelfth", "twelve Drummers Drumming, ")
+]
 
+def recite(start_verse, end_verse):
+    return [get_verse(num) for num in range(start_verse, end_verse + 1)]
 
-def recite(start_verse: int, end_verse: int) -> list:          
-    result = []
-    for i in range(start_verse, end_verse + 1):
-        result.append(f"On the {song[i][0]} day of Christmas my true love gave to me: {verse(i)}")
-    return result
+def get_verse(num):
+    verses = []
+    if num == 1:
+        verses = poems[0][1]
+    else:
+        verses = [poem[1] for poem in poems[num-1:0:-1]]
+        verses.append(f"and {poems[0][1]}")
 
+    opening = f"On the {poems[num - 1][0]} day of Christmas my true love gave to me: "
+    return opening + ''.join(verses)
 
-def verse(n: int) -> str:    
-    return ' '.join([song[i][1] for i in range(n, 0, -1)])
+print(recite(12, 12))

73c4f5158aa9075082e5acfa1b556238249a01f4
Zasda Yusuf Mikail <zasdaym@gmail.com>
2020-05-10 16:47:19 +0700

    Update go Isogram solution

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -9,15 +9,16 @@ import (
 func IsIsogram(s string) bool {
 	checked := map[rune]bool{}
 	for _, c := range s {
-		if unicode.IsLetter(c) {
-			char := unicode.ToLower(c)
-			if checked[char] {
-				return false
-			}
-
-			checked[char] = true
+		if !unicode.IsLetter(c) {
+			continue
 		}
 
+		char := unicode.ToLower(c)
+		if checked[char] {
+			return false
+		}
+
+		checked[char] = true
 	}
 
 	return true

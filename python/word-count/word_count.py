f78ad5bec6867f0c488df67dd75e5f614c95e3a6
Zasda Yusuf Mikail <zasdaym@gmail.com>
2020-05-22 16:29:34 +0700

    Update python Word Count solution
    Signed-off-by: Zasda Yusuf Mikail <zasdaym@gmail.com>

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -4,8 +4,6 @@ def count_words(sentence):
     words = re.findall(r'[0-9]+|[a-z]+\'?[a-z]+|[a-z]+', sentence.lower())
     counts = {}
     for word in words:
-        if word in counts:
-            counts[word] += 1
-        else:
-            counts[word] = 1
+        counts.setdefault(word, 0)
+        counts[word] += 1
     return counts

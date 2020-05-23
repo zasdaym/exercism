7ef36f1e24d726748986b911dce8779ae537dfeb
Zasda Yusuf Mikail <zasdaym@gmail.com>
2020-05-23 16:50:07 +0700

    Update go Luhn solution
    Signed-off-by: Zasda Yusuf Mikail <zasdaym@gmail.com>

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -3,41 +3,35 @@ package luhn
 
 import (
 	"strings"
-	"unicode"
+	"strconv"
 )
 
 // Valid determines if given numbers is valid per the Luhn formula.
 func Valid(numbers string) bool {
-	// convert number string to slice of runes
 	runes := []rune(strings.ReplaceAll(numbers, " ", ""))
-	// edge case
 	if len(runes) <= 1 {
 		return false
 	}
 
 	total := 0
-	toDouble := true
+	toDouble := len(runes)%2 == 0
 
-	// iterate from back to front
-	for i := len(runes) - 1; i >= 0; i-- {
-		// second number will be doubled flag
-		toDouble = !toDouble
-		// if not number, false immediately
-		if !unicode.IsNumber(runes[i]) {
+	for _, r := range runes {
+		num, err := strconv.Atoi(string(r))
+		if err != nil {
 			return false
 		}
 
-		num := int(runes[i] - '0')
-		// double every two number
 		if toDouble {
 			num *= 2
 			if num > 9 {
 				num -= 9
 			}
 		}
-		// add to total
+
 		total += num
+		toDouble = !toDouble
 	}
-	// if total divisible by 10, then true
+
 	return total%10 == 0
 }

2953999ba3408c0a7c70213f63003f1b0c37892b
Zasda Yusuf Mikail <zasdaym@gmail.com>
2020-06-09 10:42:09 +0700

    Add go Parallel Letter Frequency solution
    Signed-off-by: Zasda Yusuf Mikail <zasdaym@gmail.com>

---
+++ go/parallel-letter-frequency/parallel_letter_frequency.go
@@ -0,0 +1,46 @@
+// Package letter is solution for problem Parallel Letter Frequency.
+package letter
+
+import (
+	"sync"
+)
+
+// FreqMap records the frequency of each rune in a given text.
+type FreqMap map[rune]int
+
+type freqMapMutex struct {
+	freqMap FreqMap
+	sync.Mutex
+}
+
+// Frequency counts the frequency of each rune in a given text and returns this
+// data as a FreqMap.
+func Frequency(s string) FreqMap {
+	m := FreqMap{}
+	for _, r := range s {
+		m[r]++
+	}
+	return m
+}
+
+func ConcurrentFrequency(texts []string) FreqMap {
+	m := freqMapMutex{
+		freqMap: FreqMap{},
+	}
+	var wg sync.WaitGroup
+	for _, text := range texts {
+		wg.Add(1)
+		go countLetter(text, &m, &wg)
+	}
+	wg.Wait()
+	return m.freqMap
+}
+
+func countLetter(text string, m *freqMapMutex, wg *sync.WaitGroup) {
+	for _, r := range text {
+		m.Lock()
+		m.freqMap[r]++
+		m.Unlock()
+	}
+	wg.Done()
+}

8987f33f4b6ad09b11e656a72bc78cf7dab8e63b
Zasda Yusuf Mikail <zasdaym@gmail.com>
2020-06-09 10:00:57 +0700

    Update go Clock solution
    Signed-off-by: Zasda Yusuf Mikail <zasdaym@gmail.com>

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -5,27 +5,37 @@ import (
 	"fmt"
 )
 
+const day = 1440
+
 // Clock represents a clock time.
-type Clock int
+type Clock struct {
+	minutes int
+}
 
 // New returns new clock from given hours and minutes.
 func New(hours, minutes int) Clock {
-	return Clock(mod((hours*60 + minutes), 1440))
+	return Clock{
+		minutes: mod((hours*60 + minutes), day),
+	}
 }
 
 // String returns string format of Clock.
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c/60, c%60)
+	return fmt.Sprintf("%02d:%02d", c.minutes/60, c.minutes%60)
 }
 
 // Add returns Clock added with given minutes.
 func (c Clock) Add(minutes int) Clock {
-	return Clock(mod(int(c)+minutes, 1440))
+	return Clock{
+		minutes: mod(int(c.minutes)+minutes, day),
+	}
 }
 
 // Subtract returns Clock subtracted with given minutes.
 func (c Clock) Subtract(minutes int) Clock {
-	return Clock(mod(int(c)-minutes, 1440))
+	return Clock{
+		minutes: mod(int(c.minutes)-minutes, day),
+	}
 }
 
 func mod(x, m int) int {

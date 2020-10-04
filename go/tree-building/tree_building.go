9fc610c122f13607e5505c4dfc4328ad7abaff99
Zasda Yusuf Mikail <zasdaym@gmail.com>
2020-10-04 13:14:59 +0700

    Update go Tree Building solution
    Signed-off-by: Zasda Yusuf Mikail <zasdaym@gmail.com>

--- go/tree-building/tree_building.go
+++ go/tree-building/tree_building.go
@@ -3,6 +3,7 @@ package tree
 
 import (
 	"fmt"
+	"sort"
 )
 
 const rootID = 0
@@ -32,27 +33,19 @@ var (
 	ErrInvalidParent = fmt.Errorf("node has invalid parent")
 )
 
-// Build returns a root node of built tree from given records.
+// Build creates a Tree built from given Records and returns its root node.
 func Build(records []Record) (*Node, error) {
 	// edge case for zero record
 	if len(records) == 0 {
 		return nil, nil
 	}
 
-	// sort given node records by its ID
-	positions := make([]int, len(records))
-	for i, r := range records {
-		if r.ID < rootID || r.ID >= len(records) {
-			return nil, ErrInvalidRecordID
-		}
+	// sort given records by ID
+	sort.Slice(records, func(i, j int) bool { return records[i].ID < records[j].ID })
 
-		positions[r.ID] = i
-	}
-
-	// range over sorted node records
+	// range over sorted records
 	nodes := make([]Node, len(records))
-	for i := range positions {
-		r := records[positions[i]]
+	for i, r := range records {
 		// check if record ID doesn't match the sorted index
 		if r.ID != i {
 			return nil, ErrNonContinuousNode
@@ -67,9 +60,9 @@ func Build(records []Record) (*Node, error) {
 		// append current record to tree
 		nodes[i].ID = i
 		if i != rootID {
-			// add current record as children of parent node
-			p := &nodes[r.Parent]
-			p.Children = append(p.Children, &nodes[i])
+			// add current record as children of its parent
+			parent := &nodes[r.Parent]
+			parent.Children = append(parent.Children, &nodes[i])
 		}
 	}
 

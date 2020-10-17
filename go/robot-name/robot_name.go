6f41f95a59bab478700861e31754701d2d52fec8
Zasda Yusuf Mikail <zasdaym@gmail.com>
2020-10-17 20:43:46 +0700

    Add go Robot Name solution
    Signed-off-by: Zasda Yusuf Mikail <zasdaym@gmail.com>

---
+++ go/robot-name/robot_name.go
@@ -0,0 +1,52 @@
+//Package robotname is solution for problem Robot Name.
+package robotname
+
+import (
+	"fmt"
+)
+
+// Robot represents a robot with a name.
+type Robot struct {
+	name string
+}
+
+// Name returns name of the robot and generates a new one if the robot has no name.
+func (r *Robot) Name() (string, error) {
+	if r.name != "" {
+		return r.name, nil
+	}
+	name, err := randomName()
+	if err != nil {
+		return "", err
+	}
+	r.name = name
+	return r.name, nil
+}
+
+var counter = 0
+
+func randomName() (string, error) {
+	if counter > 675999 {
+		return "", fmt.Errorf("all combinations used")
+	}
+	counter++
+	return (generateLetters(counter) + generateNumber(counter)), nil
+}
+
+func generateLetters(counter int) string {
+	n := (counter / 1000) + 65
+	first := string(rune(n / 65))
+	second := string(rune(n % 65))
+	return first + second
+}
+
+func generateNumber(counter int) string {
+	n := (counter % 1000)
+	number := fmt.Sprintf("%03d", n)
+	return number
+}
+
+// Reset clears Robot's name.
+func (r *Robot) Reset() {
+	r.name = ""
+}

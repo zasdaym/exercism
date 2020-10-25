e3bd2f93caf8e513182b5923f0ef099f8badd420
Zasda Yusuf Mikail <zasdaym@gmail.com>
2020-10-25 19:41:28 +0700

    Add go Tournament solution
    Signed-off-by: Zasda Yusuf Mikail <zasdaym@gmail.com>

---
+++ go/tournament/tournament.go
@@ -0,0 +1,121 @@
+package tournament
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"sort"
+	"strings"
+)
+
+type Team struct {
+	name   string
+	played int
+	win    int
+	loss   int
+	draw   int
+	point  int
+}
+
+func (t Team) String() string {
+	return fmt.Sprintf("%-30s | %2d | %2d | %2d | %2d | %2d", t.name, t.played, t.win, t.draw, t.loss, t.point)
+}
+
+type Result []*Team
+
+func (r Result) Len() int {
+	return len(r)
+}
+
+func (r Result) Less(i, j int) bool {
+	if r[i].point == r[j].point {
+		return strings.Compare(r[i].name, r[j].name) > 0
+	}
+	return r[i].point < r[j].point
+}
+
+func (r Result) Swap(i, j int) {
+	r[i], r[j] = r[j], r[i]
+}
+
+type Teams map[string]*Team
+
+// Tally read teams matches from a io.Reader and prints the result to the given io.Writer.
+func Tally(r io.Reader, w io.Writer) error {
+	teams := make(Teams)
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		line := scanner.Text()
+		match := strings.Split(line, ";")
+		if match[0] == "" {
+			continue
+		}
+		if match[0][0] == '#' {
+			continue
+		}
+		if err := checkMatch(match); err != nil {
+			return err
+		}
+		processMatch(teams, match)
+	}
+	result := generateResult(teams)
+	return printResult(w, result)
+}
+
+func generateResult(teams Teams) Result {
+	var result Result
+	for _, team := range teams {
+		result = append(result, team)
+	}
+	sort.Sort(sort.Reverse(result))
+	return result
+}
+
+func checkMatch(match []string) error {
+	if len(match) != 3 {
+		return fmt.Errorf("wrong match format")
+	}
+	if match[2] != "win" && match[2] != "loss" && match[2] != "draw" {
+		return fmt.Errorf("invalid result, must be win or loss")
+	}
+	return nil
+}
+
+func processMatch(teams Teams, match []string) {
+	if _, ok := teams[match[0]]; !ok {
+		teams[match[0]] = &Team{name: match[0]}
+	}
+	if _, ok := teams[match[1]]; !ok {
+		teams[match[1]] = &Team{name: match[1]}
+	}
+	home, away := teams[match[0]], teams[match[1]]
+	home.played++
+	away.played++
+	switch match[2] {
+	case "win":
+		home.win++
+		home.point += 3
+		away.loss++
+	case "loss":
+		home.loss++
+		away.win++
+		away.point += 3
+	case "draw":
+		home.draw++
+		home.point++
+		away.draw++
+		away.point++
+	}
+}
+
+func printResult(w io.Writer, result Result) error {
+	if _, err := w.Write([]byte("Team                           | MP |  W |  D |  L |  P\n")); err != nil {
+		return err
+	}
+	for _, team := range result {
+		if _, err := w.Write([]byte(fmt.Sprintf("%s\n", team))); err != nil {
+			return err
+		}
+	}
+	return nil
+}

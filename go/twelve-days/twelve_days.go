44fa05728c1e015e3141dd973388e1fe80c4c7a3
Zasda Yusuf Mikail <zasdaym@gmail.com>
2020-11-01 09:30:34 +0700

    Update go Twelve Days solution
    Signed-off-by: Zasda Yusuf Mikail <zasdaym@gmail.com>

--- go/twelve-days/twelve_days.go
+++ go/twelve-days/twelve_days.go
@@ -15,61 +15,61 @@ type Lyric struct {
 var lyrics = []Lyric{
 	{
 		day:  "first",
-		text: "a Partridge in a Pear Tree",
+		text: "a Partridge in a Pear Tree.",
 	},
 	{
 		day:  "second",
-		text: "two Turtle Doves",
+		text: "two Turtle Doves, and ",
 	},
 	{
 		day:  "third",
-		text: "three French Hens",
+		text: "three French Hens, ",
 	},
 	{
 		day:  "fourth",
-		text: "four Calling Birds",
+		text: "four Calling Birds, ",
 	},
 	{
 		day:  "fifth",
-		text: "five Gold Rings",
+		text: "five Gold Rings, ",
 	},
 	{
 		day:  "sixth",
-		text: "six Geese-a-Laying",
+		text: "six Geese-a-Laying, ",
 	},
 	{
 		day:  "seventh",
-		text: "seven Swans-a-Swimming",
+		text: "seven Swans-a-Swimming, ",
 	},
 	{
 		day:  "eighth",
-		text: "eight Maids-a-Milking",
+		text: "eight Maids-a-Milking, ",
 	},
 	{
 		day:  "ninth",
-		text: "nine Ladies Dancing",
+		text: "nine Ladies Dancing, ",
 	},
 	{
 		day:  "tenth",
-		text: "ten Lords-a-Leaping",
+		text: "ten Lords-a-Leaping, ",
 	},
 	{
 		day:  "eleventh",
-		text: "eleven Pipers Piping",
+		text: "eleven Pipers Piping, ",
 	},
 	{
 		day:  "twelfth",
-		text: "twelve Drummers Drumming",
+		text: "twelve Drummers Drumming, ",
 	},
 }
 
 // Song returns a full twelve days song.
 func Song() string {
-	result := make([]string, 0)
+	song := make([]string, 0)
 	for i := range lyrics {
-		result = append(result, Verse(i+1))
+		song = append(song, Verse(i+1))
 	}
-	return strings.Join(result, "\n")
+	return strings.Join(song, "\n")
 }
 
 // Verse returns from 1 to n-th verses of the twelve days song.
@@ -77,16 +77,8 @@ func Verse(n int) string {
 	var result strings.Builder
 	begin := fmt.Sprintf("On the %s day of Christmas my true love gave to me: ", lyrics[n-1].day)
 	result.WriteString(begin)
-	if n == 1 {
-		s := fmt.Sprintf("%s.", lyrics[0].text)
-		result.WriteString(s)
-		return result.String()
-	}
 	for i := n - 1; i >= 0; i-- {
-		s := fmt.Sprintf("%s, ", lyrics[i].text)
-		if i == 0 {
-			s = fmt.Sprintf("and %s.", lyrics[i].text)
-		}
+		s := fmt.Sprintf("%s", lyrics[i].text)
 		result.WriteString(s)
 	}
 	return result.String()

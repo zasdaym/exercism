0d157750208261126573b660fe956aadc0a0ca2d
Zasda Yusuf Mikail <zasdaym@gmail.com>
2021-09-15 21:46:24 +0700

    Add go Two Fer solution

---
+++ go/two-fer/two_fer.go
@@ -0,0 +1,12 @@
+// Package twofer provides Two Fer solution.
+package twofer
+
+import "fmt"
+
+// ShareWith should have a comment documenting it.
+func ShareWith(name string) string {
+	if name == "" {
+		name = "you"
+	}
+	return fmt.Sprintf("One for %s, one for me.", name)
+}

be9ad96f5f5d710d5e5aed2f3d69370833f913e3
Zasda Yusuf Mikail <zasdaym@gmail.com>
2021-09-24 17:49:51 +0700

    Update ruby Series solution

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,12 +1,15 @@
+# frozen_string_literal: true
+
 class Series
   attr_reader :digits
 
   def initialize(digits)
     @digits = digits
   end
-  
-  def slices(n)
-    raise ArgumentError.new('n cannot be bigger than digits length') if n > digits.length
-    digits.slice(0..digits.length-n).chars.map.with_index { |_, i| digits.slice(i, n) }
+
+  def slices(length)
+    raise ArgumentError, 'slice length cannot be bigger than digits length' if length > digits.length
+
+    digits.slice(0..digits.length - length).chars.map.with_index { |_, i| digits.slice(i, length) }
   end
 end

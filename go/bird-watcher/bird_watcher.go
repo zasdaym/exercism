a8bc38f823a094aa7affbbcdf0add0868879c8b5
Zasda Yusuf Mikail <zasdaym@gmail.com>
2021-10-30 17:38:39 +0700

    Add go Bird Watcher

---
+++ go/bird-watcher/bird_watcher.go
@@ -0,0 +1,35 @@
+// Package birdwatcher provides Bird Watcher solultion.
+package birdwatcher
+
+// TotalBirdCount return the total bird count by summing
+// the individual day's counts.
+func TotalBirdCount(birdsPerDay []int) int {
+	var sum int
+	for _, v := range birdsPerDay {
+		sum += v
+	}
+	return sum
+}
+
+// BirdsInWeek returns the total bird count by summing
+// only the items belonging to the given week.
+func BirdsInWeek(birdsPerDay []int, week int) int {
+	var sum int
+	start := 7 * (week - 1)
+	for _, v := range birdsPerDay[start : start+7] {
+		sum += v
+	}
+	return sum
+}
+
+// FixBirdCountLog returns the bird counts after correcting
+// the bird counts for alternate days.
+func FixBirdCountLog(birdsPerDay []int) []int {
+	for i := range birdsPerDay {
+		if i%2 == 0 {
+			birdsPerDay[i] += 1
+		}
+	}
+
+	return birdsPerDay
+}

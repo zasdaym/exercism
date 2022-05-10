4934968db3a4bd524f4fecf40b1a5b6d574e0eaf
Zasda Yusuf Mikail <zasdaym@gmail.com>
2022-05-10 23:21:49 +0700

    Add nim grade-school

---
+++ nim/grade-school/grade_school.nim
@@ -0,0 +1,18 @@
+import algorithm, sequtils
+
+type
+  Student = tuple
+    name: string
+    grade: int
+
+  School* = object
+    students*: seq[Student]
+
+proc sortedRoster(students: seq[Student]): seq[Student] =
+  students.sortedByIt((it.grade, it.name))
+
+proc roster*(s: School): seq[string] =
+  sortedRoster(s.students).mapIt(it.name)
+
+proc grade*(s: School, grade: int): seq[string] =
+  sorted(s.students.filterIt(it.grade == grade).mapIt(it.name))

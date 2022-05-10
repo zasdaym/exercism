a26c3bbcc396b11be2cea0e9deb526870ae86e04
Zasda Yusuf Mikail <zasdaym@gmail.com>
2022-05-10 22:45:28 +0700

    Add nim sum-of-multiples

---
+++ nim/sum-of-multiples/sum_of_multiples.nim
@@ -0,0 +1,7 @@
+import sequtils
+
+proc sum*(n: int, divisors: seq[int]): int =
+  result = 0
+  for i in countup(1, n - 1):
+    if any(divisors, proc (x: int): bool = x != 0 and i mod x == 0):
+      result += i

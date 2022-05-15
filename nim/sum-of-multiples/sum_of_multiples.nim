855d91c84af42584bb01e9bc8c9dafe376a6cfbc
Zasda Yusuf Mikail <zasdaym@gmail.com>
2022-05-15 22:06:51 +0700

    Update nim sum-of-multiples

--- nim/sum-of-multiples/sum_of_multiples.nim
+++ nim/sum-of-multiples/sum_of_multiples.nim
@@ -1,7 +1,7 @@
-import std/sequtils
+import std/[sequtils, sugar]
 
 proc sum*(n: int, divisors: seq[int]): int =
   result = 0
-  for i in countup(1, n - 1):
-    if any(divisors, proc (x: int): bool = x != 0 and i mod x == 0):
+  for i in 1..n-1:
+    if any(divisors, x => x != 0 and i mod x == 0):
       result += i

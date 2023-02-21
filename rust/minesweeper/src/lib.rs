c01904adefd57ff8d1681a4348bc0380c66187cd
Zasda Yusuf Mikail <zasdaym@gmail.com>
2023-02-21 21:44:21 +0700

    Fix rust minesweeper

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -1,16 +1,39 @@
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    vec![]
+    let height = minefield.len() as i32;
+    (0..height)
+        .map(|i| {
+            let width = minefield[i as usize].len() as i32;
+            (0..width)
+                .map(|j| {
+                    if minefield[i as usize].as_bytes()[j as usize] == b'*' {
+                        '*'
+                    } else {
+                        match neighbors(i, j)
+                            .iter()
+                            .copied()
+                            .filter(|&(y, x)| y >= 0 && x >= 0 && y < height && x < width)
+                            .filter(|&(y, x)| minefield[y as usize].as_bytes()[x as usize] == b'*')
+                            .count()
+                        {
+                            0 => ' ',
+                            n => (n as u8 + '0' as u8) as char,
+                        }
+                    }
+                })
+                .collect()
+        })
+        .collect()
 }
 
-fn neighbor_coordinates(i: usize, j: usize) -> Vec<(usize, usize)> {
+fn neighbors(y: i32, x: i32) -> Vec<(i32, i32)> {
     vec![
-        (i - 1, j - 1),
-        (i - 1, j),
-        (i - 1, j + 1),
-        (i, j - 1),
-        (i, j + 1),
-        (i + 1, j - 1),
-        (i + 1, j),
-        (i + 1, j + 1),
+        (y - 1, x - 1),
+        (y - 1, x),
+        (y - 1, x + 1),
+        (y, x - 1),
+        (y, x + 1),
+        (y + 1, x - 1),
+        (y + 1, x),
+        (y + 1, x + 1),
     ]
 }

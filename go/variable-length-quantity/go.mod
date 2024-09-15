bb3b3b2897d6c7bbf9663a048f066972a3233ee6
Zasda Yusuf Mikail <zasdaym@gmail.com>
2024-09-15 22:34:22 +0700

    Add go variable-length-quantity

--- go/variable-length-quantity/go.mod
+++ go/variable-length-quantity/go.mod
@@ -1,3 +1,5 @@
 module variablelengthquantity
 
-go 1.16
+go 1.22.0
+
+toolchain go1.23.1
--- go/variable-length-quantity/variable_length_quantity.go
+++ go/variable-length-quantity/variable_length_quantity.go
@@ -1,32 +1,69 @@
 package variablelengthquantity
 
+import (
+	"fmt"
+)
+
 func EncodeVarint(input []uint32) []byte {
-	var result []byte
-	for i := 0; i < len(input); i++ {
-		encoded := encode(input[i])
+	result := make([]byte, 0)
+	for _, v := range input {
+		encoded := encodeInt(v)
 		result = append(result, encoded...)
 	}
 	return result
 }
 
-// encode n with variable length encoding.
-// Only works for number between 0 and 127.
-func encode(n uint32) []byte {
+func encodeInt(n uint32) []byte {
+	if n == 0 {
+		return []byte{0}
+	}
+
 	var result []byte
 	for n > 0 {
-		// get the last 7 right bits
-		encoded := n & 0x7f
-		// shift n to the right by 7 bits
+		// Get 7 rightmost bits
+		part := n & 0x7f
+		// Shift 7 bits to the right
 		n >>= 7
-		// if there are more bits to encode, set the 8th bit to 1
-		if n > 0 {
-			encoded |= 0x80
-		}
-		result = append(result, byte(encoded))
+		result = append(result, byte(part))
+	}
+
+	// Reverse
+	for i, j := 0, len(result)-1; i < j; i, j = i+1, j-1 {
+		result[i], result[j] = result[j], result[i]
+	}
+
+	// All bytes except the last one should have MSB set to 1
+	for i := 0; i < len(result)-1; i++ {
+		result[i] |= 0x80
 	}
 	return result
 }
 
 func DecodeVarint(input []byte) ([]uint32, error) {
-	panic("Please implement the EncodeVarint function")
+	var (
+		result       []uint32
+		current      uint32
+		fullSequence bool
+	)
+
+	for _, octet := range input {
+		// Add the 7 rightmost bits to the temporary accumulator
+		current = (current << 7) | uint32(octet&0x7f)
+
+		// Not last octet? Move on
+		if octet&0x80 != 0 {
+			continue
+		}
+
+		// Last octet handling
+		result = append(result, current)
+		current = 0
+		fullSequence = true
+	}
+
+	if !fullSequence {
+		return nil, fmt.Errorf("incomplete sequences")
+	}
+
+	return result, nil
 }

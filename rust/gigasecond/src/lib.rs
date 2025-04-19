be73ebb6e8ae16f90f21abc24e35daa382682386
Zasda Yusuf Mikail <zasdaym@gmail.com>
2025-04-19 11:29:46 +0700

    Reset rust progress

--- rust/gigasecond/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use std::ops::Add;
-
-use time::{Duration, PrimitiveDateTime as DateTime};
-
-// Returns a DateTime one billion seconds after start.
-pub fn after(start: DateTime) -> DateTime {
-    start.add(Duration::seconds(1000000000))
-}

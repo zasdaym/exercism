9c8a6c837f1b57c51f567822a467ffa8d118b4a7
Zasda Yusuf Mikail <zasdaym@gmail.com>
2025-04-22 15:52:18 +0700

    Add rust clock

---
+++ rust/clock/src/lib.rs
@@ -0,0 +1,29 @@
+use std::fmt;
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
+
+impl Clock {
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        let total_minutes = hours * 60 + minutes;
+        let normalized_minutes = ((total_minutes % (24 * 60)) + 24 * 60) % (24 * 60);
+
+        Clock {
+            hours: (normalized_minutes / 60) % 24,
+            minutes: normalized_minutes % 60,
+        }
+    }
+
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        Clock::new(self.hours, self.minutes + minutes)
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+    }
+}

44e279c0448b6b1a65f00b473941e1a42489c10f
Zasda Yusuf Mikail <zasdaym@gmail.com>
2025-04-22 15:58:25 +0700

    Update rust clock

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,29 +1,30 @@
 use std::fmt;
 
+const MINUTES_IN_A_DAY: i32 = 24 * 60;
+
 #[derive(Debug, Eq, PartialEq)]
 pub struct Clock {
-    hours: i32,
     minutes: i32,
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
         let total_minutes = hours * 60 + minutes;
-        let normalized_minutes = ((total_minutes % (24 * 60)) + 24 * 60) % (24 * 60);
+        let normalized_minutes =
+            ((total_minutes % MINUTES_IN_A_DAY) + MINUTES_IN_A_DAY) % (MINUTES_IN_A_DAY);
 
         Clock {
-            hours: (normalized_minutes / 60) % 24,
-            minutes: normalized_minutes % 60,
+            minutes: normalized_minutes,
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        Clock::new(self.hours, self.minutes + minutes)
+        Clock::new(0, self.minutes + minutes)
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+        write!(f, "{:02}:{:02}", self.minutes / 60, self.minutes % 60)
     }
 }

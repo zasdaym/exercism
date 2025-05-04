35565c20a151556069a33769beebef2c24681485
Zasda Yusuf Mikail <zasdaym@gmail.com>
2025-05-04 20:42:51 +0700

    Add ruby dnd-character

---
+++ ruby/dnd-character/dnd_character.rb
@@ -0,0 +1,24 @@
+class DndCharacter
+  attr_reader :charisma, :dexterity, :hitpoints, :intelligence, :strength, :wisdom, :constitution
+
+  def self.modifier(constitution)
+    (constitution - 10) / 2
+  end
+
+  def initialize
+    @constitution = ability
+    @charisma = ability
+    @dexterity = ability
+    @intelligence = ability
+    @strength = ability
+    @wisdom = ability
+    @hitpoints = DndCharacter.modifier(@constitution) + 10
+  end
+
+  private
+
+  def ability
+    dice = Array.new(4) { rand(1..6) }
+    dice.sum - dice.min
+  end
+end
